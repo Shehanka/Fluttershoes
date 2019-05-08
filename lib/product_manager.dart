dccabcba048086cdd0192742170cb43ed4ed4ca8
Shehanka <hcsperera@outlook.com>
2019-05-09 02:30:28 +0530

    Update

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -16,6 +16,12 @@ class _ProductManagerState extends State<ProductManager> {
   List<String> _products = [];
 
   @override
+  void initState() {
+    _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(children: [
       Container(
